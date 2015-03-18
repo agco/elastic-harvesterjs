c793d5d814e203b85ed7b75492bc2f9dc972966e
Stephen Sebro <Stephen@brownianm.com>
2015-03-18 07:31:21 -0500

    Worked on bug in test suite

--- test/app.js
+++ test/app.js
@@ -1,10 +1,8 @@
 var harvest = require('harvesterjs');
-var RSVP = require('rsvp');
 var ElasticHarvest = require('../elastic-harvester');
 
 function createApp(options) {
     var harvestApp = harvest(options)
-
         .resource('person', {
             name: String,
             appearances: Number,
@@ -12,18 +10,15 @@ function createApp(options) {
             soulmate: {ref: 'person', inverse: 'soulmate'},
             lovers: [{ref: 'person', inverse: 'lovers'}]
         })
-
         .resource('pet', {
             name: String,
             appearances: Number,
             toys:['toy'],
             friends:['pet']
         })
-
         .resource('toy', {
             name: String
         });
-
 
     var peopleSearch = new ElasticHarvest(harvestApp, options.es_url,options.es_index, "people");
     var indexReadyPromise = peopleSearch.deleteIndex().then(function(){
@@ -33,26 +28,16 @@ function createApp(options) {
     });
 
 
-    harvestApp.router.get('/people/search', peopleSearch.route);
-
-    harvestApp.onRouteCreated('person').then(function(harvestRoute){
-        peopleSearch.setHarvestRoute(harvestRoute);
-    });
+    peopleSearch.setHarvestRoute(harvestApp.route('person'));
     peopleSearch.enableAutoSync("person");
 
-    return RSVP.all([
-        harvestApp.onRouteCreated('pet'),
-        harvestApp.onRouteCreated('person'),
-        indexReadyPromise
-    ])
+    harvestApp.router.get('/people/search', peopleSearch.route);
+
+    return indexReadyPromise
         .then(function () {
             harvestApp.listen(process.env.PORT);
             return harvestApp;
         });
 }
-
-
-
-
 
 module.exports = createApp;
