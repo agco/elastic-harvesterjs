94e5f164d0fd108c54ed2525740f26b64a2648fd
Stephen Sebro <Stephen@brownianm.com>
2015-03-18 15:55:30 -0500

    Circumvented a dependency issue between harvest and elastic-harvest.

--- test/app.js
+++ test/app.js
@@ -2,7 +2,16 @@ var harvest = require('harvesterjs');
 var ElasticHarvest = require('../elastic-harvester');
 
 function createApp(options) {
-    var harvestApp = harvest(options)
+
+    var harvestApp = harvest(options);
+    var peopleSearch;
+    var elasticHarvestRoute;
+    //This circumvents a dependency issue between harvest and elastic-harvest.
+    harvestApp.router.get('/people/search', function(){
+        elasticHarvestRoute.apply(peopleSearch,arguments);
+    });
+
+    harvestApp
         .resource('person', {
             name: String,
             appearances: Number,
@@ -20,7 +29,9 @@ function createApp(options) {
             name: String
         });
 
-    var peopleSearch = new ElasticHarvest(harvestApp, options.es_url,options.es_index, "people");
+    peopleSearch = new ElasticHarvest(harvestApp, options.es_url,options.es_index, "people");
+    elasticHarvestRoute = peopleSearch.route;
+
     var indexReadyPromise = peopleSearch.deleteIndex().then(function(){
         return peopleSearch.initializeMapping(require("./test.mapping.js")).then(function(response){
             console.log('Initializing ES mapping: ' + JSON.stringify(response));
@@ -30,8 +41,6 @@ function createApp(options) {
 
     peopleSearch.setHarvestRoute(harvestApp.route('person'));
     peopleSearch.enableAutoSync("person");
-
-    harvestApp.router.get('/people/search', peopleSearch.route);
 
     return indexReadyPromise
         .then(function () {
